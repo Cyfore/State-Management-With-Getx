b808f0b3f9176b3ca18f6ed166435a714c07f359
Emirhan Ülker <emr.brk.265@gmail.com>
2022-11-23 16:25:17 +0300

    abstractAndjson

--- lib/architecture/moduls/home/pages/home_page/controllers/home_page_controller.dart
+++ lib/architecture/moduls/home/pages/home_page/controllers/home_page_controller.dart
@@ -1,3 +1,24 @@
 import 'package:get/get.dart';
+import 'package:state_management_with_getx/architecture/global/abstracts/future_abstract.dart';
 
-class HomeMainPageViewController extends GetxController {}
+class HomeMainPageViewController extends GetxController implements IFutureAbstract {
+  var postsFuture = Future.value().obs;
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    updateMainFuture();
+  }
+
+  @override
+  void assignFuture(List data) {
+    // TODO: implement assignFuture
+  }
+
+  @override
+  void updateMainFuture() {
+    // TODO: implement updateMainFuture
+  }
+
+  void getHomePost() {}
+}
